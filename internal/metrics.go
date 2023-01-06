d27016788bbb1028f2f86aaa5273179846efd5bc
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-01-06 20:14:00 +0100

    fix: do not write 'go_' prefixed metrics to file

--- internal/metrics.go
+++ internal/metrics.go
@@ -7,6 +7,7 @@ import (
 	"github.com/prometheus/common/expfmt"
 	"github.com/rs/zerolog/log"
 	"io/ioutil"
+	"strings"
 )
 
 const (
@@ -70,8 +71,11 @@ func dumpMetrics() (string, error) {
 	}
 
 	for _, f := range families {
-		if err := enc.Encode(f); err != nil {
-			log.Info().Msgf("could not encode metric: %s", err.Error())
+		// Writing these metrics will cause a duplication error with other tools writing the same metrics
+		if !strings.HasPrefix(f.GetName(), "go_") {
+			if err := enc.Encode(f); err != nil {
+				log.Info().Msgf("could not encode metric: %s", err.Error())
+			}
 		}
 	}
 
