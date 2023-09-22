357f64de3f70f911a160eb4b148d0dd816c74b96
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-09-22 17:39:28 +0100

    refactor: move config to own package

---
+++ internal/config/conf.go
@@ -0,0 +1,98 @@
+package config
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	log "github.com/rs/zerolog/log"
+	"github.com/soerenschneider/vault-ssh-cli/pkg"
+)
+
+var sensitiveVars = map[string]struct{}{
+	FLAG_VAULT_AUTH_APPROLE_ROLE_ID:   {},
+	FLAG_VAULT_AUTH_APPROLE_SECRET_ID: {},
+	FLAG_VAULT_AUTH_TOKEN:             {},
+}
+
+type Config struct {
+	subcmd string
+
+	VaultAddress      string `mapstructure:"vault-address"  validate:"required"`
+	VaultToken        string `mapstructure:"vault-auth-token"`
+	VaultRoleId       string `mapstructure:"vault-auth-approle-role-id"`
+	VaultSecretId     string `mapstructure:"vault-auth-approle-secret-id"`
+	VaultSecretIdFile string `mapstructure:"vault-auth-approle-secret-id-file"`
+	VaultMountApprole string `mapstructure:"vault-auth-approle-mount"`
+	VaultAuthImplicit bool   `mapstructure:"vault-auth-implicit"`
+	VaultMountSsh     string `mapstructure:"vault-ssh-mount"  validate:"required"`
+	VaultSshRole      string `mapstructure:"vault-ssh-role" validate:"required"`
+
+	ForceNewSignature                      bool    `mapstructure:"force-new-signature"`
+	CertificateLifetimeThresholdPercentage float32 `mapstructure:"renew-threshold-percent" validate:""`
+
+	PublicKeyFile string `mapstructure:"pub-key-file" validate:"omitempty,file"`
+	SignedKeyFile string `mapstructure:"signed-key-file" validate:"omitempty,filepath"`
+
+	CaFile string `mapstructure:"ca-file" validate:"omitempty,filepath"`
+
+	MetricsFile string `mapstructure:"metrics-file" validate:"omitempty,filepath"`
+	Debug       bool   `mapstructure:"debug"`
+}
+
+func (c *Config) SetSubcmd(subcmd string) {
+	c.subcmd = subcmd
+}
+
+func (c *Config) ExpandPaths() {
+	if len(c.PublicKeyFile) > 0 {
+		c.PublicKeyFile = pkg.GetExpandedFile(c.PublicKeyFile)
+	}
+
+	if len(c.SignedKeyFile) == 0 && len(c.PublicKeyFile) > 0 {
+		auto := strings.Replace(c.PublicKeyFile, ".pub", "", 1)
+		auto = pkg.GetExpandedFile(fmt.Sprintf("%s-cert.pub", auto))
+		log.Info().Msgf("Automatically derived value for '%s' (%s) from supplied '%s' (%s)", FLAG_SIGNED_KEY_FILE, auto, FLAG_PUBKEY_FILE, c.PublicKeyFile)
+		c.SignedKeyFile = auto
+	}
+}
+
+func (c *Config) ValidateCommon() []error {
+	errs := make([]error, 0)
+
+	if len(c.VaultAddress) == 0 {
+		errs = append(errs, fmt.Errorf("empty '%s' provided", FLAG_VAULT_ADDRESS))
+	}
+
+	if len(c.VaultMountSsh) == 0 {
+		errs = append(errs, fmt.Errorf("empty '%s' provided", FLAG_VAULT_SSH_MOUNT))
+	}
+
+	return errs
+}
+
+var (
+	validate = validator.New()
+)
+
+// ValidateSignCommand is called for commands that actually sign a public key.
+func (c *Config) ValidateSignCommand() error {
+	return validate.Struct(c)
+}
+
+func (c *Config) Print() {
+	log.Debug().Msg("Active config values:")
+	val := reflect.ValueOf(c).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		if !val.Field(i).IsZero() {
+			fieldName := val.Type().Field(i).Tag.Get("mapstructure")
+			_, isSensitive := sensitiveVars[fieldName]
+			if isSensitive {
+				log.Debug().Msgf("%s=*** (redacted)", fieldName)
+			} else {
+				log.Debug().Msgf("%s=%v", fieldName, val.Field(i))
+			}
+		}
+	}
+}
---
+++ internal/config/flags.go
@@ -0,0 +1,29 @@
+package config
+
+const (
+	FLAG_VAULT_ADDRESS                     = "vault-address"
+	FLAG_VAULT_AUTH_TOKEN                  = "vault-auth-token" // #nosec: G101
+	FLAG_VAULT_AUTH_APPROLE_ROLE_ID        = "vault-auth-approle-role-id"
+	FLAG_VAULT_AUTH_APPROLE_SECRET_ID      = "vault-auth-approle-secret-id"      // #nosec: G101
+	FLAG_VAULT_AUTH_APPROLE_SECRET_ID_FILE = "vault-auth-approle-secret-id-file" // #nosec: G101
+	FLAG_VAULT_AUTH_APPROLE_MOUNT          = "vault-auth-approle-mount"          // #nosec: G101
+	FLAG_VAULT_AUTH_APPROLE_MOUNT_DEFAULT  = "approle"
+	FLAG_VAULT_AUTH_IMPLICIT               = "vault-auth-implicit"
+	FLAG_VAULT_SSH_MOUNT                   = "vault-ssh-mount"
+	FLAG_VAULT_SSH_MOUNT_DEFAULT           = "ssh"
+	FLAG_VAULT_SSH_ROLE                    = "vault-ssh-role"
+
+	FLAG_FORCE_NEW_SIGNATURE                = "force-new-signature"
+	FLAG_FORCE_NEW_SIGNATURE_DEFAULT        = false
+	FLAG_RENEW_THRESHOLD_PERCENTAGE         = "renew-threshold-percent"
+	FLAG_RENEW_THRESHOLD_PERCENTAGE_DEFAULT = 33.
+
+	FLAG_CA_FILE         = "ca-file"
+	FLAG_PUBKEY_FILE     = "pub-key-file"
+	FLAG_SIGNED_KEY_FILE = "signed-key-file"
+	FLAG_CONFIG_FILE     = "config-file"
+
+	FLAG_METRICS_FILE         = "metrics-file"
+	FLAG_METRICS_FILE_DEFAULT = "/var/lib/node_exporter/vault-ssh-cli.prom"
+	FLAG_DEBUG                = "debug"
+)
