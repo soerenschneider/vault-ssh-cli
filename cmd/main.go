b6191ef1092d1a151ca13535addfede65462990e
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-09-22 17:43:31 +0100

    refactor: move flag definition

--- cmd/main.go
+++ cmd/main.go
@@ -21,31 +21,6 @@ const (
 	cliName               = "vault-ssh-cli"
 	envPrefix             = "VAULT_SSH_CLI"
 	defaultConfigFilename = "config"
-
-	FLAG_VAULT_ADDRESS                     = "vault-address"
-	FLAG_VAULT_AUTH_TOKEN                  = "vault-auth-token" // #nosec: G101
-	FLAG_VAULT_AUTH_APPROLE_ROLE_ID        = "vault-auth-approle-role-id"
-	FLAG_VAULT_AUTH_APPROLE_SECRET_ID      = "vault-auth-approle-secret-id"      // #nosec: G101
-	FLAG_VAULT_AUTH_APPROLE_SECRET_ID_FILE = "vault-auth-approle-secret-id-file" // #nosec: G101
-	FLAG_VAULT_AUTH_APPROLE_MOUNT          = "vault-auth-approle-mount"
-	FLAG_VAULT_AUTH_APPROLE_MOUNT_DEFAULT  = "approle"
-	FLAG_VAULT_AUTH_IMPLICIT               = "vault-auth-implicit"
-	FLAG_VAULT_SSH_MOUNT                   = "vault-ssh-mount"
-	FLAG_VAULT_SSH_MOUNT_DEFAULT           = "ssh"
-	FLAG_VAULT_SSH_ROLE                    = "vault-ssh-role"
-
-	FLAG_FORCE_NEW_SIGNATURE                = "force-new-signature"
-	FLAG_FORCE_NEW_SIGNATURE_DEFAULT        = false
-	FLAG_RENEW_THRESHOLD_PERCENTAGE         = "renew-threshold-percent"
-	FLAG_RENEW_THRESHOLD_PERCENTAGE_DEFAULT = 33.
-
-	FLAG_CA_FILE         = "ca-file"
-	FLAG_PUBKEY_FILE     = "pub-key-file"
-	FLAG_SIGNED_KEY_FILE = "signed-key-file"
-	FLAG_CONFIG_FILE     = "config-file"
-
-	FLAG_METRICS_FILE         = "metrics-file"
-	FLAG_METRICS_FILE_DEFAULT = "/var/lib/node_exporter/vault-ssh-cli.prom"
 )
 
 func main() {
@@ -70,15 +45,16 @@ func main() {
 		},
 	}
 
-	root.PersistentFlags().StringP(FLAG_VAULT_ADDRESS, "a", "", "Vault instance to connect to. If not specified, falls back to env var VAULT_ADDR.")
-	root.PersistentFlags().StringP(FLAG_VAULT_AUTH_TOKEN, "t", "", "Vault token to use for authentication. Can not be used in conjunction with AppRole login data.")
-	root.PersistentFlags().BoolP(FLAG_VAULT_AUTH_IMPLICIT, "i", false, "Try to implicitly authenticate to vault using VAULT_TOKEN env var or ~/.vault-token file.")
-	root.PersistentFlags().StringP(FLAG_VAULT_AUTH_APPROLE_ROLE_ID, "", "", "Vault role_id to use for AppRole login. Can not be used in conjuction with Vault token flag.")
-	root.PersistentFlags().StringP(FLAG_VAULT_AUTH_APPROLE_SECRET_ID, "", "", "Vault secret_id to use for AppRole login. Can not be used in conjuction with Vault token flag.")
-	root.PersistentFlags().StringP(FLAG_VAULT_AUTH_APPROLE_SECRET_ID_FILE, "", "", "Flat file to read Vault secret_id from. Can not be used in conjuction with Vault token flag.")
-	root.PersistentFlags().StringP(FLAG_VAULT_AUTH_APPROLE_MOUNT, "", FLAG_VAULT_AUTH_APPROLE_MOUNT_DEFAULT, "Path where the AppRole auth method is mounted.")
-	root.PersistentFlags().StringP(FLAG_VAULT_SSH_MOUNT, "m", FLAG_VAULT_SSH_MOUNT_DEFAULT, "Path where the PKI secret engine is mounted.")
-	root.PersistentFlags().StringP(FLAG_CONFIG_FILE, "c", "", "File to read the config from")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_ADDRESS, "a", "", "Vault instance to connect to. If not specified, falls back to env var VAULT_ADDR.")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_AUTH_TOKEN, "t", "", "Vault token to use for authentication. Can not be used in conjunction with AppRole login data.")
+	root.PersistentFlags().BoolP(config.FLAG_VAULT_AUTH_IMPLICIT, "i", false, "Try to implicitly authenticate to vault using VAULT_TOKEN env var or ~/.vault-token file.")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_AUTH_APPROLE_ROLE_ID, "", "", "Vault role_id to use for AppRole login. Can not be used in conjuction with Vault token flag.")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_AUTH_APPROLE_SECRET_ID, "", "", "Vault secret_id to use for AppRole login. Can not be used in conjuction with Vault token flag.")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_AUTH_APPROLE_SECRET_ID_FILE, "", "", "Flat file to read Vault secret_id from. Can not be used in conjuction with Vault token flag.")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_AUTH_APPROLE_MOUNT, "", config.FLAG_VAULT_AUTH_APPROLE_MOUNT_DEFAULT, "Path where the AppRole auth method is mounted.")
+	root.PersistentFlags().StringP(config.FLAG_VAULT_SSH_MOUNT, "m", config.FLAG_VAULT_SSH_MOUNT_DEFAULT, "Path where the PKI secret engine is mounted.")
+	root.PersistentFlags().StringP(config.FLAG_CONFIG_FILE, "c", "", "File to read config from")
+	root.PersistentFlags().BoolP(config.FLAG_DEBUG, "", false, "Set loglevel to debug")
 
 	root.AddCommand(readCaCertCmd())
 	root.AddCommand(getSignHostKeyCmd())
@@ -90,17 +66,17 @@ func main() {
 	}
 }
 
-func config() (*Config, error) {
-	viper.SetDefault(FLAG_VAULT_SSH_MOUNT, FLAG_VAULT_SSH_MOUNT_DEFAULT)
-	viper.SetDefault(FLAG_VAULT_AUTH_APPROLE_MOUNT, FLAG_VAULT_AUTH_APPROLE_MOUNT_DEFAULT)
+func getConfig() (*config.Config, error) {
+	viper.SetDefault(config.FLAG_VAULT_SSH_MOUNT, config.FLAG_VAULT_SSH_MOUNT_DEFAULT)
+	viper.SetDefault(config.FLAG_VAULT_AUTH_APPROLE_MOUNT, config.FLAG_VAULT_AUTH_APPROLE_MOUNT_DEFAULT)
 
 	viper.SetConfigName(defaultConfigFilename)
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("$HOME/.config/vault-ssh-cli")
 	viper.AddConfigPath("/etc/vault-ssh-cli/")
 
-	if viper.IsSet(FLAG_CONFIG_FILE) {
-		configFile := viper.GetString(FLAG_CONFIG_FILE)
+	if viper.IsSet(config.FLAG_CONFIG_FILE) {
+		configFile := viper.GetString(config.FLAG_CONFIG_FILE)
 		log.Info().Msgf("Trying to read config from '%s'", configFile)
 		viper.SetConfigFile(configFile)
 	}
