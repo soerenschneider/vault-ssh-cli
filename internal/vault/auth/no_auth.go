96666d16ac7363cb3bd0a21fa503da43134b1ca8
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-01-04 16:40:11 +0100

    fix: fix implicit auth

--- internal/vault/auth/no_auth.go
+++ internal/vault/auth/no_auth.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"os"
 
 	"github.com/hashicorp/vault/api"
 )
@@ -10,5 +11,9 @@ type NoAuth struct {
 }
 
 func (t *NoAuth) Login(ctx context.Context, client *api.Client) (*api.Secret, error) {
-	return &api.Secret{}, nil
+	return &api.Secret{
+		Auth: &api.SecretAuth{
+			ClientToken: os.Getenv("VAULT_TOKEN"),
+		},
+	}, nil
 }
